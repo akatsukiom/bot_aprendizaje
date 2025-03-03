b97c1990f9edec847dae5f103af3dfcb0786f29d
akatsukiom <akatsukiomeg@gmail.com>
2025-03-02 20:59:25 -0600

    learning moduloe

--- db.js
+++ db.js
@@ -2,7 +2,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-// Ruta a la base de datos
+// Ruta de la base de datos
 const dbPath = path.join(__dirname, 'mensajes.db');
 
 // Crear o conectar la DB
@@ -11,13 +11,14 @@ const db = new sqlite3.Database(dbPath, (err) => {
         console.error("❌ Error al conectar con la base de datos:", err.message);
     } else {
         console.log("📂 Base de datos SQLite conectada");
-        // Crear tabla mensajes si no existe
+        // Crear tabla mensajes (incluyendo fromMe)
         db.run(`
             CREATE TABLE IF NOT EXISTS mensajes (
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
                 remitente TEXT,
                 mensaje TEXT,
-                fecha TEXT
+                fecha TEXT,
+                fromMe INTEGER DEFAULT 0
             )
         `);
     }
